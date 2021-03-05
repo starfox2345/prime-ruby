a501615bbbe24594130f7c263673e5bd0d11c529
yatsun <yatsunlam25@gmail.com>
2021-03-05 14:10:15 -0500

    commit tree 6af2b83845541577ef69550ed2a35b84587c9b3a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+    #prime_nums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+    if num < 2
+        false
+    elsif num == 2
+        true
+    else
+        (2..num/2).none? { |i| num % i == 0}
+    end
+end
